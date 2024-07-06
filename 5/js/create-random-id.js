03f0d0a772d656db18661627ad26f63f7e3f6ecb
Keks <keks@htmlacademy.ru>
2024-07-07 01:10:00 +0300

    :heavy_check_mark: Сборка #5

---
+++ 5/js/create-random-id.js
@@ -0,0 +1,19 @@
+import { getRandomInteger } from './get-random-integer';
+
+function createRandomIdFromRangeGenerator(min, max) {
+  const previousValues = [];
+
+  return function () {
+    let currentValue = getRandomInteger(min, max);
+    if (previousValues.length >= (max - min + 1)) {
+      return null;
+    }
+    while (previousValues.includes(currentValue)) {
+      currentValue = getRandomInteger(min, max);
+    }
+    previousValues.push(currentValue);
+    return currentValue;
+  };
+}
+
+export { createRandomIdFromRangeGenerator };
---
+++ 5/js/main.js
@@ -0,0 +1,5 @@
+import './functions';
+import { createArrayPhoto } from './create-array-photo';
+
+// eslint-disable-next-line no-console
+console.log(createArrayPhoto);
