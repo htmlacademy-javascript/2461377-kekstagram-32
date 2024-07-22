d27c8802dd69824063754607dd4fb025ad65f70e
Keks <keks@htmlacademy.ru>
2024-07-23 00:21:46 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,6 @@
+import { getArrayPhoto } from './array-photo.js';
+import { generateThumbnails } from './picture.js';
+
+generateThumbnails(getArrayPhoto());
+// eslint-disable-next-line no-console
+// console.log(getArrayPhoto());
