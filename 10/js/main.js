7203d35e4e6a6bde3cf3cef4530c4f1a813f8f94
Keks <keks@htmlacademy.ru>
2024-08-04 01:21:23 +0300

    :heavy_check_mark: Сборка #10

---
+++ 10/js/main.js
@@ -0,0 +1,11 @@
+import { getArrayPhoto } from './array-photo.js';
+// import { generateThumbnails } from './picture.js';
+import { renderGallery } from './gallery.js';
+// import { onBigPicture } from './big-picture.js';
+import './form.js';
+
+renderGallery(getArrayPhoto());
+// generateThumbnails(getArrayPhoto());
+// onBigPicture();
+// eslint-disable-next-line no-console
+// console.log(getArrayPhoto());
