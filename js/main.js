9263b0c86fee2f3efcfce381874a10e4c26d948b
DrZhuker <drzhuker@gmail.com>
2024-08-04 01:19:15 +0300

    module9-task1

--- js/main.js
+++ js/main.js
@@ -2,9 +2,10 @@ import { getArrayPhoto } from './array-photo.js';
 // import { generateThumbnails } from './picture.js';
 import { renderGallery } from './gallery.js';
 // import { onBigPicture } from './big-picture.js';
+import './form.js';
 
 renderGallery(getArrayPhoto());
 // generateThumbnails(getArrayPhoto());
 // onBigPicture();
 // eslint-disable-next-line no-console
-console.log(getArrayPhoto());
+// console.log(getArrayPhoto());
