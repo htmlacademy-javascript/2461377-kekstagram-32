715dad726f9e0348916d2a453545a768a32518c8
Keks <keks@htmlacademy.ru>
2024-08-07 00:23:35 +0300

    :heavy_check_mark: Сборка #13

---
+++ 13/js/big-picture.js
@@ -0,0 +1,91 @@
+const COMMENTS_PORTION = 5;
+
+const bigPictureELement = document.querySelector('.big-picture');
+// const commentCountElement = bigPictureELement.querySelector('.social__comment-count');
+const commentShownCountElement = bigPictureELement.querySelector('.social__comment-shown-count');
+const commentTotalCountElement = bigPictureELement.querySelector('.social__comment-total-count');
+const commentListElement = bigPictureELement.querySelector('.social__comments');
+const commentsLoaderElement = bigPictureELement.querySelector('.comments-loader');
+const bodyElement = document.querySelector('body');
+const cancelButtonElement = bigPictureELement.querySelector('.big-picture__cancel');
+const commentElement = document.querySelector('#comment').content.querySelector('.social__comment');
+
+let commentsShown = 0;
+let comments = [];
+
+const createComment = ({ avatar, name, message }) => {
+  const comment = commentElement.cloneNode(true);
+
+  comment.querySelector('.social__picture').src = avatar;
+  comment.querySelector('.social__picture').alt = name;
+  comment.querySelector('.social__text').textContent = message;
+
+  return comment;
+};
+
+const renderComments = () => {
+  commentsShown += COMMENTS_PORTION;
+
+  if (commentsShown >= comments.length) {
+    commentsLoaderElement.classList.add('hidden');
+    commentsShown = comments.length;
+  } else {
+    commentsLoaderElement.classList.remove('hidden');
+  }
+
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < commentsShown; i++) {
+    const comment = createComment(comments[i]);
+    fragment.append(comment);
+  }
+
+  commentListElement.innerHTML = '';
+  commentListElement.append(fragment);
+  commentShownCountElement.textContent = commentsShown;
+  commentTotalCountElement.textContent = comments.length;
+};
+
+const hideBigPicture = () => {
+  bigPictureELement.classList.add('hidden');
+  bodyElement.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  commentsShown = 0;
+};
+
+function onDocumentKeydown(evt) {
+  if (evt.key === 'Escape') {
+    evt.preventDefault();
+    hideBigPicture();
+  }
+}
+
+const onCancelButtonClick = () => {
+  hideBigPicture();
+};
+
+const onCommentLoaderClick = () => renderComments();
+
+const renderPictureDetails = ({ url, likes, description }) => {
+  bigPictureELement.querySelector('.big-picture__img img').src = url;
+  bigPictureELement.querySelector('.big-picture__img img').alt = description;
+  bigPictureELement.querySelector('.likes-count').textContent = likes;
+  bigPictureELement.querySelector('.social__caption').textContent = description;
+};
+
+const showBigPicture = (data) => {
+  bigPictureELement.classList.remove('hidden');
+  bodyElement.classList.add('modal-open');
+  commentsLoaderElement.classList.add('hidden');
+  document.addEventListener('keydown', onDocumentKeydown);
+
+  renderPictureDetails(data);
+  comments = data.comments;
+  if (comments.length > 0) {
+    renderComments();
+  }
+};
+
+cancelButtonElement.addEventListener('click', onCancelButtonClick);
+commentsLoaderElement.addEventListener('click', onCommentLoaderClick);
+
+export { showBigPicture };
---
+++ 13/js/util.js
@@ -0,0 +1,54 @@
+const ALERT_SHOW_TIME = 5000;
+
+const dataErrorTemplate = document.querySelector('#data-error').content.querySelector('.data-error');
+
+const showAlert = () => {
+  const dataErrorElement = dataErrorTemplate.cloneNode(true);
+  document.body.append(dataErrorElement);
+
+  setTimeout(() => {
+    dataErrorElement.remove();
+  }, ALERT_SHOW_TIME);
+};
+
+const debounce = (callback, timeoutDelay = 500) => {
+  let timeoutId;
+  return (...rest) => {
+    clearTimeout(timeoutId);
+    timeoutId = setTimeout(() => callback.apply(this, rest), timeoutDelay);
+  };
+};
+
+export { showAlert, debounce };
+// const getRandomInteger = (a, b) => {
+//   const lower = Math.ceil(Math.min(a, b));
+//   const upper = Math.floor(Math.max(a, b));
+//   const result = Math.random() * (upper - lower + 1) + lower;
+//   return Math.floor(result);
+// };
+
+// function createRandomIdFromRangeGenerator(min, max) {
+//   const previousValues = [];
+
+//   return function () {
+//     let currentValue = getRandomInteger(min, max);
+//     if (previousValues.length >= (max - min + 1)) {
+//       return null;
+//     }
+//     while (previousValues.includes(currentValue)) {
+//       currentValue = getRandomInteger(min, max);
+//     }
+//     previousValues.push(currentValue);
+//     return currentValue;
+//   };
+// }
+
+// const createIdGenerator = () => {
+//   let lastGenerator = 0;
+//   return () => {
+//     lastGenerator += 1;
+//     return lastGenerator;
+//   };
+// };
+
+// export { createRandomIdFromRangeGenerator, getRandomInteger, createIdGenerator };
